fee0be806c38271ab7e52f7ceb661666be4eda55
Skysoldier <skysoldier98@gmail.com>
2023-09-16 00:00:27 +1000

    Q2

--- climate_q2.py
+++ climate_q2.py
@@ -1,9 +1,24 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import openpyxl as op
+import csv
+
+#Read excel --- why were we taught about openpyxl when it doesn't open csv?
+
+#sheet = op.load_workbook(r'climate.csv')
 
 years = []
 co2 = []
 temp = []
+
+#open the csv file and read it, append result by line
+with open('climate.csv') as csvdata:
+    reader = csv.reader(csvdata, delimiter=',')
+    for r in reader:
+        years.append(r[0])
+        co2.append(r[1])
+        temp.append(r[2])
+
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
@@ -17,4 +32,3 @@ plt.ylabel("Temp (C)")
 plt.xlabel("Year (decade)") 
 plt.show() 
 plt.savefig("co2_temp_2.png") 
-
